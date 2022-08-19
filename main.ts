d7a4e99d353778a3c5183b3c67faa6ef6e1d4862
tom <584633421@qq.com>
2022-08-19 09:59:19 +0800

    123

--- main.ts
+++ main.ts
@@ -1,29 +1,31 @@
-export enum Gesture {
-    //% block="tilt up"
-    tiltup = 3,
-    //% block="shake"
-    shake = 1,
-    //% block="tilt left"
-    tiltleft=2,
-    //% block="tilt right"
-    liltright = 23,
-    //% block="face up"
-    faceup = 21,
-    //% block="face down"
-    facedown = 123,
 
-}
-
-export enum get1 {
-    //% block="A"
-    A = 32,
-    //% block="B"
-    B = 56,
-
-}
 //% color=#8600FF weight=100 icon"\uf516"
 //% block="makecode_test" blockID="makecode_test"
 namespace makecode_test {
+    
+    export enum Gesture {
+        //% block="tilt up"
+        tiltup = 3,
+        //% block="shake"
+        shake = 1,
+        //% block="tilt left"
+        tiltleft = 2,
+        //% block="tilt right"
+        liltright = 23,
+        //% block="face up"
+        faceup = 21,
+        //% block="face down"
+        facedown = 123,
+    
+    }
+    
+    export enum get1 {
+        //% block="A"
+        A = 32,
+        //% block="B"
+        B = 56,
+    
+    }
 
     //% block = "abc_1 Get3 = $abc_a + $abc_A"
     //% gesture.fieldEditor="gridpicker"
